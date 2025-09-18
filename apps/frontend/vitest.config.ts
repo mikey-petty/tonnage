897020f2640f64e2c1a602c42715fcc3d28a97df
mpetty9991 <47963967+mpetty9991@users.noreply.github.com>
2025-09-18 09:05:34 -0400

    feat: setup e2e testing

--- apps/frontend/vitest.config.ts
+++ apps/frontend/vitest.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+    exclude: ["**/e2e/**", "**/node_modules/**"],
+  },
   server: {
     host: "0.0.0.0",
     port: 5173,
