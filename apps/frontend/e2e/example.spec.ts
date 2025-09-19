929b48d5cf891d5b36dd5a689f181df3d8e4e5d6
mpetty9991 <47963967+mpetty9991@users.noreply.github.com>
2025-09-19 10:45:42 -0400

    fix: linting

--- apps/frontend/e2e/example.spec.ts
+++ apps/frontend/e2e/example.spec.ts
@@ -2,12 +2,12 @@ import { test, expect } from "@playwright/test";
 
 test("has url", async ({ page }) => {
   await page.route("**/api/hello/", (route) =>
-    route.fulfill({ json: { message: "Hello from mock!" } })
+    route.fulfill({ json: { message: "Hello from mock!" } }),
   );
   await page.goto(process.env.PLAYWRIGHT_WEB_URL || "http://localhost:5173");
 
   await expect(page).toHaveURL(
-    process.env.PLAYWRIGHT_WEB_URL || "http://localhost:5173"
+    process.env.PLAYWRIGHT_WEB_URL || "http://localhost:5173",
   );
 
   const button = await page.getByTestId("count");
