030d691ac1e33a69bbc361f7e87e5b7acbd906d2
mpetty9991 <47963967+mpetty9991@users.noreply.github.com>
2025-09-19 10:28:02 -0400

    chore: abstract out env variables

--- apps/frontend/playwright.config.ts
+++ apps/frontend/playwright.config.ts
@@ -26,9 +26,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    // baseURL: process.env.PLAYWRIGHT_BASE_URL || "http://localhost:5173",
     trace: "on-first-retry",
   },
 
@@ -72,8 +70,8 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "npm run dev",
-    url: "http://localhost:5173",
+    command: process.env.PLAYWRIGHT_WEB_COMMAND || "npm run dev",
+    url: process.env.PLAYWRIGHT_WEB_URL || "http://localhost:5173",
     reuseExistingServer: !process.env.CI,
   },
 });
