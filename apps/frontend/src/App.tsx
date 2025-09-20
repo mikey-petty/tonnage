1115a1e5ee2b1e34eaabfd7a1814ccba2ddf6340
mpetty9991 <47963967+mpetty9991@users.noreply.github.com>
2025-09-20 00:45:00 -0400

    fix: config files

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -17,7 +17,7 @@ function App() {
   return (
     <>
       <div>
-        <h1>Frontend + Backend Example</h1>
+        <h1>Frontend + Backend Examples</h1>
         <p>{message}</p>
       </div>
       <div>
