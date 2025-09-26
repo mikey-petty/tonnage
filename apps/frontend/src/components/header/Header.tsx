366f21c8a70eace866aa411eed4ce19aa104fd96
mpetty9991 <47963967+mpetty9991@users.noreply.github.com>
2025-09-25 23:07:18 -0400

    fix: header component filename casing

---
+++ apps/frontend/src/components/header/Header.tsx
@@ -0,0 +1,8 @@
+import styles from "./Header.module.css";
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <h1 className={styles.title}>Tonnage</h1>
+    </header>
+  );
+}
