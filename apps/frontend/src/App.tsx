d980f002b9528db698d540511a6d8622081980ff
mpetty9991 <47963967+mpetty9991@users.noreply.github.com>
2025-09-25 23:07:52 -0400

    feat: replace header text with Header component in App

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,13 +1,13 @@
-import { useState } from "react";
 import "./App.css";
 import BackendMessage from "./components/backend-message/BackendMessage";
+import Header from "./components/header/Header";
 import SQLiteMessage from "./components/sqlite-message/SQLiteMessage";
 
 function App() {
   return (
     <>
       <div>
-        <h1>Frontend + Backend Examples</h1>
+        <Header />
         <BackendMessage />
         <SQLiteMessage />
       </div>
