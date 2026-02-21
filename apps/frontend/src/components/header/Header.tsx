3533e5872fe934cea0551dfe5ed6be65ba6bc589
mpetty9991 <47963967+mpetty9991@users.noreply.github.com>
2026-02-20 22:29:11 -0500

    feat: update header component structure and styling; add SQLite message table creation logic

--- apps/frontend/src/components/header/Header.tsx
+++ apps/frontend/src/components/header/Header.tsx
@@ -1,8 +1,10 @@
 import styles from "./Header.module.css";
 export default function Header() {
   return (
-    <header className={styles.header}>
-      <h1 className={styles.title}>Tonnage</h1>
-    </header>
+    <div className={styles.div__header}>
+      <header className={styles.header}>
+        <h1 className={styles.title}>Tonnages</h1>
+      </header>
+    </div>
   );
 }
--- apps/frontend/src/components/sqlite-message/SQLiteMessage.tsx
+++ apps/frontend/src/components/sqlite-message/SQLiteMessage.tsx
@@ -1,14 +1,25 @@
 import { useState } from "react";
 import { db } from "../../storage/sqlite-promiser-loader";
+import { CREATE_MESSAGES_TABLE, INSERT_MESSAGE } from "../../storage/tables";
 
 export default function SQLiteMessage() {
   const [version, setVersion] = useState("Not Loaded Yet");
   const handleClick = async () => {
+    await db("exec", {
+      sql: CREATE_MESSAGES_TABLE,
+    });
+
+    await db("exec", {
+      sql: INSERT_MESSAGE,
+      bind: ["Hello World"],
+    });
+
     const result = await db("exec", {
-      sql: "SELECT sqlite_version() AS version",
+      sql: /*sql*/ `SELECT * FROM messages LIMIT 1`,
       rowMode: "object",
     });
-    const version = result.result.resultRows?.[0].version;
+
+    const version = result.result.resultRows?.[0].content;
     setVersion(version);
   };
 
